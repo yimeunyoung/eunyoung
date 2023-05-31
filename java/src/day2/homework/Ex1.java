00f1ec713a0b99a66d5d904166d17f322c7f1c54
yimeunyoung <kil_yea@hanmail.net>
2023-05-31 15:14:39 +0900

    day2 숙제

---
+++ java/src/day2/homework/Ex1.java
@@ -0,0 +1,9 @@
+package day2.homework;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
---
+++ java/src/day2/homework/Ex2.java
@@ -0,0 +1,9 @@
+package day2.homework;
+
+public class Ex2 {
+
+	public static void main(String[] args) {
+
+	}
+
+}
