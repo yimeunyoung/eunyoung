49badedcd0017c9099393891f3b5c8add8124250
yimeunyoung <kil_yea@hanmail.net>
2023-06-01 13:37:40 +0900

    자바 2일차 연산자 연습문제

---
+++ java/src/day3/practice/Ex1.java
@@ -0,0 +1,27 @@
+package day3.practice;
+
+import java.util.Scanner;
+
+public class Ex1 {
+
+	public static void main(String[] args) {
+		/*성적을 입력받아 입력받은 성적이 60점 이상인지 확인하는 코드를 작성하세요.
+		 * 예:
+		 * input score : 
+		 * 65
+		 * Do you pass 65 points? true
+		 * input score :
+		 * 45
+		 * Do you pass 45 points? false
+		 */
+		Scanner sc = new Scanner(System.in);
+		System.out.println("input score : ");
+		int score = sc.nextInt();
+		boolean result = score >= 60;
+		System.out.println("Do you pass " + score + " points? " + result);
+		sc.close();
+		
+
+	}
+
+}
