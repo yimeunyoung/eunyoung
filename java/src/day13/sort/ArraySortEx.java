0c83f23cf6de1d78e475c7dc868d4d43dce35d7a
yimeunyoung <kil_yea@hanmail.net>
2023-06-16 15:12:29 +0900

    자바 : 정렬 예제

--- java/src/day13/sort/ArraySortEx.java
+++ java/src/day13/sort/ArraySortEx.java
@@ -10,7 +10,7 @@ public class ArraySortEx {
 	public static void main(String[] args) {
 		//버블 정렬 예제
 		int arr[] = {1, 4, 7, 9, 2, 8, 3, 6};
-		
+		Array.printArray(arr);
 		for(int i = 0; i < arr.length-1 ; i++) { //arr.length-1에서 -1을 안해줘도 되지만 하는 이유는 맨 마지막 숫자는 굳이 안해도 1개만 남아있기 때문에 비교 안하면 더 빠르게 끝날 수 있기때문 
 			for(int j = 0; j < arr.length-1-i ; j++) { //-i를 해주면 반복횟수를 1번이라도 줄일 수 있다.
 				if(arr[j] > arr[j+1]) { // 부등호 방향을 바꾸면 오름차순/내림차순 설정 가능
