3de201b591c45d9b9a3bab589fa223c339151fd7
yimeunyoung <kil_yea@hanmail.net>
2023-06-16 15:12:16 +0900

    자바 : 성적 클래스 예제

---
+++ java/src/day13/practice/Subject.java
@@ -0,0 +1,55 @@
+package day13.practice;
+
+import lombok.Data;
+
+/*성적을 관리하기 위한 Subject 클래스를 만드세요. */
+
+@Data
+public class Subject { 
+	private String title; // 과목명 : 국어, 영어, 수학
+	private double total; // 과목의 총점 = 중간 + 기말 + 수행평가
+	private double midTerm;
+	private double finalTerm;
+	private double performance;
+	private int grade;
+	private int semester;
+	
+	//생성자
+	public Subject(String title) {
+		this.title = title;
+		this.grade = 1;
+		this.semester = 1;
+	}
+	
+	public Subject(String title, int grade, int semester) {
+		this.title = title;
+		this.grade = grade;
+		this.semester = semester;
+	}
+	
+	//메서드
+	/** 중간고사 성적 추가/수정 : setMidTerm */
+	
+	/** 기말고사 성적 추가/수정 : setFinalTerm */
+	
+	/** 수행평가 성적 추가/수정 : setPerformance */
+	
+	/** 성적 추가(전체)/수정 */
+	public void updateScore(int midTerm, int finalTerm, int Performance) {
+		this.midTerm = midTerm;
+		this.finalTerm = finalTerm;
+		this.performance = performance;
+		total = midTerm + finalTerm + performance;
+	}
+	/**성적 출력*/
+	public void print() {
+		System.out.println("title     : " + title);
+		System.out.println("grade     : " + grade);
+		System.out.println("semester  : " + semester);
+		System.out.println("mid       : " + midTerm);
+		System.out.println("final     : " + finalTerm);
+		System.out.println("performance  : " + performance);
+		System.out.println("total     : " + total);
+	}
+	
+}
