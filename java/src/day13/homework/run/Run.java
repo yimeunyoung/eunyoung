eb32dc77d942b0277a126fc8ecab02a8ee492897
yimeunyoung <kil_yea@hanmail.net>
2023-06-19 11:45:21 +0900

    자바 : 13일차 과제 단어장 예제 풀이

---
+++ java/src/day13/homework/run/Run.java
@@ -0,0 +1,12 @@
+package day13.homework.run;
+
+import day13.homework.controller.VocabularyNoteController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		VocabularyNoteController controller = new VocabularyNoteController();
+		controller.run(); //controller에서 run이라는 메서드를 실행되도록 설정
+	}
+
+}
