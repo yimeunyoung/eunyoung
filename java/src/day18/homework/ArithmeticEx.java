2b78d23816ac4d221265ba12122c91f31ac254dd
yimeunyoung <kil_yea@hanmail.net>
2023-06-26 08:46:47 +0900

    자바 : 이미지 파일 과제 예제(18일차 과제2)

---
+++ java/src/day18/homework/ArithmeticEx.java
@@ -0,0 +1,33 @@
+package day18.homework;
+
+public class ArithmeticEx {
+
+	public static void main(String[] args) {
+		/*산술연산을 하는 프로그램을 작성하세요.
+		인터페이스 이용
+		(package : day18.homework, file : ArithmeticEx)
+		public interface Program{
+			void run();
+			void printMenu();
+			void runMenu(int menu);
+		}
+		Input Integer
+		Input Operator
+		Exit
+		menu : 1
+		input : 1 2
+		1. Input Integer
+		2. Input Operator
+		3. Exit
+		menu : 2
+		input : +
+		result : 3
+		Input Integer
+		Input Operator
+		Exit
+		menu : 3
+		Exit		*/
+
+	}
+
+}
