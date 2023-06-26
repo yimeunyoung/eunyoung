85f14ac711c922c58ecb93466abe5c5812b16d91
yimeunyoung <kil_yea@hanmail.net>
2023-06-26 15:17:53 +0900

    과제 - 산술연산 메서드 만들고 테스트(예외처리 이용)

--- java/src/day19/homework/ArithmeticEx.java
+++ java/src/day19/homework/ArithmeticEx.java
@@ -1,10 +1,43 @@
 package day19.homework;
 
+import java.util.Scanner;
+
 public class ArithmeticEx {
 
 	public static void main(String[] args) {
 		//산술 연산하는 메서드를 만들고 테스트하는 코드를 작성하세요. 단 예외처리 이용
+		int num1, num2;
+		char op;
+		Scanner sc = new Scanner(System.in);
+		System.out.print("input ex) 1 + 2 : ");
+		
+		num1 = sc.nextInt();
+		op = sc.next().charAt(0);
+		num2 = sc.nextInt();
+		arithmetic(num1, num2, op);
 
+	
 	}
 
+public static void arithmetic(int num1, double num2, char op) {
+	switch(op) {
+	case '+':
+		System.out.println("" + num1 + op + num2 + " = " + (num1 + num2));
+		break;
+	case '-':
+		System.out.println("" + num1 + op + num2 + " = " + (num1 - num2));
+		break;
+	case '*':
+		System.out.println("" + num1 + op + num2 + " = " + (num1 * num2));
+		break;
+	case '/':
+		System.out.println("" + num1 + op + num2 + " = " + (num1 / (double)num2));
+		break;
+	case '%':
+		System.out.println("" + num1 + op + num2 + " = " + (num1 % num2));
+		break;
+	default:
+		System.out.println("Not operator!");
+		}
+	}
 }
