11b785ba29e7519c0b354c70c348a5da3b08fd79
yimeunyoung <kil_yea@hanmail.net>
2023-06-27 11:04:37 +0900

    자바 : 산술연산 메서드 예제(19일차 과제)

--- java/src/day19/homework/ArithmeticEx.java
+++ java/src/day19/homework/ArithmeticEx.java
@@ -1,43 +1,53 @@
 package day19.homework;
 
+import java.text.MessageFormat;
 import java.util.Scanner;
 
 public class ArithmeticEx {
 
 	public static void main(String[] args) {
-		//산술 연산하는 메서드를 만들고 테스트하는 코드를 작성하세요. 단 예외처리 이용
-		int num1, num2;
-		char op;
+
 		Scanner sc = new Scanner(System.in);
-		System.out.print("input ex) 1 + 2 : ");
-		
-		num1 = sc.nextInt();
-		op = sc.next().charAt(0);
-		num2 = sc.nextInt();
-		arithmetic(num1, num2, op);
-
-	
+		//두 정수와 산술연산자 입력
+		System.out.print("input (ex:1 + 2) :");
+		try {
+			int num1 = sc.nextInt();
+			char op = sc.next().charAt(0);
+			int num2 = sc.nextInt();
+			
+			String message = "{0} {1} {2} = {3}";
+			
+			double res = calculate(num1, op, num2);
+			
+			String result = MessageFormat.format(message, num1, op, num2, res);
+			System.out.println(result);
+		}catch(ArithmeticException e) {
+			System.out.println(e.getMessage());
+		}catch(Exception e) {
+			System.out.println("Exception!!");
+		}
+		sc.close();
 	}
-
-public static void arithmetic(int num1, double num2, char op) {
-	switch(op) {
-	case '+':
-		System.out.println("" + num1 + op + num2 + " = " + (num1 + num2));
-		break;
-	case '-':
-		System.out.println("" + num1 + op + num2 + " = " + (num1 - num2));
-		break;
-	case '*':
-		System.out.println("" + num1 + op + num2 + " = " + (num1 * num2));
-		break;
-	case '/':
-		System.out.println("" + num1 + op + num2 + " = " + (num1 / (double)num2));
-		break;
-	case '%':
-		System.out.println("" + num1 + op + num2 + " = " + (num1 % num2));
-		break;
-	default:
-		System.out.println("Not operator!");
+	public static double calculate(int num1, char op, int num2) 
+		throws ArithmeticException{
+		//연산자에 따를 결과를 출력
+		switch(op) {
+		case '+':	return num1 + num2;
+		case '-':	return num1 - num2;
+		case '*':	return num1 * num2;
+		case '/':
+			if(num2 == 0) {
+				throw new ArithmeticException("It should not be divided by zero.");
+			}
+			return num1 / (double)num2;
+		case '%':	
+			if(num2 == 0) {
+				throw new ArithmeticException("It should not be divided by zero.");
+			}
+			return num1 % num2;
+		default:
+			throw new ArithmeticException("Wrong operator");
 		}
 	}
+	
 }
