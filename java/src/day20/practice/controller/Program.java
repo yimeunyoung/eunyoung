e525666cc04ba89950a8af237daa0d697e8c6000
yimeunyoung <kil_yea@hanmail.net>
2023-06-27 14:51:01 +0900

    자바 : 학생 관리 프로그램 예제(List 이용)

---
+++ java/src/day20/practice/controller/Program.java
@@ -0,0 +1,9 @@
+package day20.practice.controller;
+
+public interface Program {
+
+	 void run();
+	 void printMenu();
+	 void runMenu(int menu);
+
+}
---
+++ java/src/day20/practice/run/StudentEx.java
@@ -0,0 +1,12 @@
+package day20.practice.run;
+
+import day20.practice.controller.StudentController;
+
+public class StudentEx {
+
+	public static void main(String[] args) {
+		StudentController sc = new StudentController();
+		sc.run();
+	}
+
+}
