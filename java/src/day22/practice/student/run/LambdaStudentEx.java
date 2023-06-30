fbac56b4124348a3f81a0e2e7efd86950d5e2b93
yimeunyoung <kil_yea@hanmail.net>
2023-06-30 11:57:56 +0900

    자바 : 학생 정보 출력 예제 패키지명 수정

---
+++ java/src/day22/practice/student/run/LambdaStudentEx.java
@@ -0,0 +1,17 @@
+package day22.practice.student.run;
+
+import day22.practice.student.controller.StudentManager3;
+
+public class LambdaStudentEx {
+
+	public static void main(String[] args) {
+		/* 학생 정보를 출력하는 기능을 구현하세요.
+		 * 1. 학생 전체 출력
+		 * 2. 학생 학년 출력
+		 * 3. 학생 검색 출력
+		 * 4. 종료1
+		 */
+		StudentManager3 std = new StudentManager3();
+		std.run();
+	}
+}
