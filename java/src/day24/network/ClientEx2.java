d6e04839755a63d127096e25db24f973f54fe672
yimeunyoung <kil_yea@hanmail.net>
2023-07-03 15:02:21 +0900

    자바 : 소켓통신 채팅 예제 - 쓰레드 이용

--- java/src/day24/network/ClientEx2.java
+++ java/src/day24/network/ClientEx2.java
@@ -6,7 +6,20 @@ import java.io.OutputStream;
 import java.net.InetSocketAddress;
 import java.net.Socket;
 import java.util.Scanner;
-
+/* 이미 port를 사용중이어서 실행이 안되는 경우(서버를 연속 두번 실행하는 경우)
+1. 원도우키를 눌러서 cmd를 검색 후 실행
+-명령 프롬프트 실행
+2. 명령어 입력
+-netstat -ano
+-netstat : 명령어
+- -ano : 옵션
+3.사용중인 포트번호를 확인
+-로컬주소부분에 xx.xxx.xxx.xxx: 포트번호
+4. 포트번호에 맞는 PID 확인
+5. 작업관리자 실행
+-작업표시줄 우클릭 -> 작업관리자 선택
+6. 세부정보 택 클릭 후 확인한 PID에 맞는 프로그램을 작업끝내기 함.
+*/
 public class ClientEx2 {
 
 	public static void main(String[] args) {
