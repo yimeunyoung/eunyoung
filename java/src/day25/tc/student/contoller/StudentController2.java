21a308b9b46695f4b6eb2c9bba5131038eb75997
yimeunyoung <kil_yea@hanmail.net>
2023-07-05 14:10:54 +0900

    자바 : 대학생 정보 관리 프로그램 예제

--- java/src/day25/tc/student/contoller/StudentController2.java
+++ java/src/day25/tc/student/contoller/StudentController2.java
@@ -44,26 +44,27 @@ public class StudentController2 {
 		default:
 		}
 	}
+	
 	public void insertStudent(Scanner sc, List<Student> list) {
 		String num, name, major;
 		Student tmp;
 		//학번(문자열, 공백이 없는)
-		System.out.println("학번 : ");
+		System.out.print("학번 : ");
 		num = sc.next();
 		//이름(문자열, 공백이 있는)
 		sc.nextLine();
-		System.out.println("이름 : ");
+		System.out.print("이름 : ");
 		name = sc.nextLine();
 		//과(문자열, 공백이 있는)
-		System.out.println("전공 : ");
+		System.out.print("전공 : ");
 		major = sc.nextLine();
 		//학생정보 리스트에 저장
 		//입력한 정보를 이용하여 학생 객체를 생성
 		tmp = new Student(num, name, major);
 		//리스트에 학생 객체를 추가
 		list.add(tmp);
-		System.out.println(list);
 	}
+	
 	public void print(List<Student> list){
 		for(Student tmp2 : list) {
 			System.out.println(tmp2);
--- java/src/day25/tc/student/contoller/StudentController3.java
+++ java/src/day25/tc/student/contoller/StudentController3.java
@@ -1,6 +1,8 @@
 package day25.tc.student.contoller;
 
+import java.io.EOFException;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
@@ -14,10 +16,11 @@ import day25.tc.student.vo.Student;
 public class StudentController3 {
 	private List<Student> list = new ArrayList<Student>();
 	private Scanner sc = new Scanner(System.in);
-	private Student st = new Student(num, name, major);
-	
+
 	public void run() {
 			int menu;
+			String fileName = "src/day25/tc.student/Student.txt";
+			load(fileName);
 			do {
 			//메뉴 출력
 			printMenu();
@@ -25,9 +28,43 @@ public class StudentController3 {
 			menu = sc.nextInt();
 			//선택된 메뉴에 따른 기능 실행
 			runMenu(menu);
-		}while(menu != 3);	
+		}while(menu != 3);
+			save(fileName);
 			sc.close();
 	}
+	
+	private void load(String fileName) {
+		try(ObjectInputStream ois 
+				= new ObjectInputStream(new FileInputStream(fileName))){
+			while(true) {
+				Student tmp = (Student)ois.readObject();
+				list.add(tmp);
+				}
+			}catch(FileNotFoundException e) {
+				System.out.println("불러올 파일이 없습니다.");
+			}catch(EOFException e) {
+				System.out.println("불러오기 완료.");
+			}catch(IOException e) {
+				e.printStackTrace();
+			}catch(ClassNotFoundException e) {
+				//ObjectInputStream을 이용하여 객체단위로 읽어올 때 클래스가 Serializable인터페이스를 구현하지 않으면 발생
+				System.out.println("Student 클래스를 찾을 수 없습니다.");
+			}
+	}
+	private void save(String fileName) { //자동 저장하려면 매개변수로 String fileName 추가
+		//학생정보를 저장 => 리스트 => 하나씩 꺼내서 저장
+		//저장 => OutputStream
+		//객체단위로 저장 => ObjectOutputStream (day23 SerializableEx1 참고)
+		try(FileOutputStream fos = new FileOutputStream("fileName"); //=> 상대경로와 절대경로 모두 가능
+				ObjectOutputStream oos = new ObjectOutputStream(fos)){
+				for(Student tmp : list) {
+				oos.writeObject(tmp);
+				}
+		}catch(IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
 	private void printMenu() {
 		System.out.println("메뉴");
 		System.out.println("1. 학생등록");
@@ -50,7 +87,7 @@ public class StudentController3 {
 		default:
 		}
 	}
-	public void insertStudent() {
+	private void insertStudent() {
 		String num, name, major;
 		Student tmp;
 		//학번(문자열, 공백이 없는)
@@ -70,29 +107,9 @@ public class StudentController3 {
 		list.add(tmp);
 		System.out.println(list);
 	}
-	public void print(){
+	private void print(){
 		for(Student tmp2 : list) {
 			System.out.println(tmp2);
-		}
-	}
-
-	public void load() {
-		try(FileInputStream fis = new FileInputStream("Student.txt");
-			ObjectInputStream ois = new ObjectInputStream(fis)){
-			st = (Student)ois.readObject();
-			
-		}catch(IOException e) {
-			e.printStackTrace();
-		}catch(ClassNotFoundException e) {
-			e.printStackTrace();
-		}
-	}
-	public void save() {
-		try(FileOutputStream fos = new FileOutputStream("Student.txt");
-				ObjectOutputStream oos = new ObjectOutputStream(fos)){
-				oos.writeObject(sc);
-		}catch(IOException e) {
-			e.printStackTrace();
 		}
 	}
 }
