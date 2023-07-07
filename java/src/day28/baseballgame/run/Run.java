7d32ab2fbbbb450e576ea70b2b2219bdd66dea79
yimeunyoung <kil_yea@hanmail.net>
2023-07-07 17:18:25 +0900

    자바 : 숫자 야구게임 예제(플레이, 기록관리, 저장/불러오기)

---
+++ java/src/day28/baseballgame/run/Run.java
@@ -0,0 +1,12 @@
+package day28.baseballgame.run;
+
+import day28.baseballgame.controller.GameController;
+
+public class Run {
+
+	public static void main(String[] args) {
+		GameController gc = new GameController();
+		gc.run();
+	}
+
+}
