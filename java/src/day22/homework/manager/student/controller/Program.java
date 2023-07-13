15238764e24e0fb5ebc6e0d7bb3223ce87b904dc
yimeunyoung <kil_yea@hanmail.net>
2023-07-13 17:30:07 +0900

    자바 : 학생관리 프로그램 - 패키지 구성 및 학생, 과목, 고등학생 클래스 추가

---
+++ java/src/day22/homework/manager/student/controller/Program.java
@@ -0,0 +1,9 @@
+package day22.homework.manager.student.controller;
+
+public interface Program {
+	void printMenu();
+	void run();
+	void runMenu(int menu);
+	void load();
+	void save();
+}
---
+++ java/src/day22/homework/manager/student/run/Run.java
@@ -0,0 +1,10 @@
+package day22.homework.manager.student.run;
+
+import java.text.ParseException;
+
+public class Run {
+
+	public static void main(String[] args) throws ParseException {
+	}
+
+}
