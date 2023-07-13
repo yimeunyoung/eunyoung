aff1e395c234545a8f73efccdb58f6a06db255fb
yimeunyoung <kil_yea@hanmail.net>
2023-07-13 17:30:37 +0900

    (Directory) 자바 : 전화번호 관리 프로그램 예제

--- java/src/day22/practice/phone/vo/PhoneBook.java
+++ java/src/day22/practice/phone/vo/PhoneBook.java
@@ -4,16 +4,17 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.function.Predicate;
 
+
 public class PhoneBook {
 
-	//전화번호 리스트 관리
+	//전화번호 리스트
 	private List<PhoneNumber> list = new ArrayList<>();
-
+	
 	//전화번호 추가
 	public boolean insertPhone(String name, String number) {
 		//이름과 전화번호를 이용하여 전화번호 객체를 생성
 		PhoneNumber pn = new PhoneNumber(name, number);
-		//전화번호가 비어 있으면 => 전화번호 패턴에 맞지 않으면 저장 X, 맞으면 저장O
+		//전화번호가 비어 있으면 => 전화번호 패턴에 맞지 않으면 저장 X
 		if(pn.getNumber().length() == 0) {
 			return false;
 		}
@@ -25,29 +26,32 @@ public class PhoneBook {
 	//전화번호 수정 : 이름
 	public boolean updateName(String name, String updateName) {
 		//name과 일치하는 전화번호를 찾아야함
-		//전화번호부에 수정할 이름에 해당하는 전화번호가 어디에 있는지 화인
+		//전화번호부에 수정할 이름에 해당하는 전화번호가 어디에 있는지 확인 
 		int index = list.indexOf(new PhoneNumber(name,""));
 		//없으면 수정이 안됨 => 메서드를 종료
 		if(index == -1) {
 			return false;
 		}
-		//찾은 전화번호의 이름을 수정
+		//찾은 전화번호의 이름을 수정 
 		//전화번호부에서 이름과 일치하는 전화번호를 가져옴
 		PhoneNumber pn = list.get(index);
 		//가져온 전화번호의 이름을 수정
 		pn.setName(updateName);
-		//list.get(index).setName(updateName); //위의 두줄을 한줄로 쓰면 이렇게 쓰면 됨
-		//기본자료형은 =을 이용하면 값을 복사하기 때문에 한쪽의 값이 바뀌어도 다른쪽은 안바뀜
-		int num1 = 1; // num1 = 1
-		int num2 = num1; // num1 = 1, num2 = 1
-		num2 = 10; //num1 = 1, num2 = 10
-		//참조변수는  =을 이용하면 주소값을 복사하기 때문에 한쪽의 값이 바뀌면 다른쪽도 바뀜(공유)
-		//예를들면 pn1에 저장된 주소가 0x1000
+		//list.get(index).setName(updateName);
+		/*
+		//기본자료형은 =을 이용하면 값을 복사하기 때문에 한쪽의 값이 바뀌어도 다른쪽은
+		//안바뀜
+		int num1 = 1;	//num1 = 1
+		int num2 = num1;//num1 = 1, num2 = 1
+		num2 = 10;		//num1 = 1, num2 = 10
+		//참조변수는 =을 이용하면 주소값을 복사하기 때문에 한쪽의 값이 바뀌면 다른쪽도 바뀜(공유)
+		//예를 들면 pn1에 저장된 주소가 0x1000
 		PhoneNumber pn1 = new PhoneNumber("a", "010-1234-5678");
 		//pn1에 저장된 주소 : 0x1000, pn2에 저장된 주소 : 0x1000
 		PhoneNumber pn2 = pn1;
 		//0x1000번지에 있는 객체의 name을 Lim으로 수정
 		pn2.setName("Lim");
+		*/
 		return true;
 	}
 	
@@ -59,10 +63,11 @@ public class PhoneBook {
 		if(index == -1) {
 			return false;
 		}
+		
 		//number가 전화번호 패턴이 맞는지 확인하기 위해서
-		//전화번호 패턴이 맞으면 전화번호 객체를 생성했을 때 전화번호가 있고, 
+		//전화번호 패턴이 맞으면 전화번호 객체를 생성 했을 때 전화번호가 있고, 
 		//패턴이 맞지 않으면 빈 문자열이 있다.
-		//있으면 name과 number를 이용하여 전화번호 객체를 생성하는 대신 checkNumber를 이용
+		//name과 number를 이용하여 전화번호 객체를 생성하는 대신 checkNumber를 이용
 		//number가 전화번호 패턴이면 전화번호를 수정
 		if(!PhoneNumber.checkNumber(number)) {
 			return false;
@@ -83,20 +88,20 @@ public class PhoneBook {
 		if(!PhoneNumber.checkNumber(number)) {
 			return false;
 		}
-		//기존에 있는 전화번호를 삭제하고 
+		//기존에 있는 전화번호를 삭제
 		list.remove(index);
 		//updateName과 number를 이용하여 전화번호 객체를 생성한 후 추가
 		pn.setName(updateName);
 		list.add(pn);
 		return true;
 	}
-	//전화번호 삭제
+	//전화번호 삭제 
 	public boolean delete(String name) {
 		//name으로 PhoneNumber 객체를 생성 후 삭제 요청
-		return list.remove(new PhoneNumber(name,""));
+		return list.remove(new PhoneNumber(name, ""));
 	}
 	//전화번호 출력
-	public void print(Predicate<PhoneNumber> p) { //람다식 적용
+	public void print(Predicate<PhoneNumber> p) {
 		for(PhoneNumber pn : list) {
 			if(p.test(pn)) {
 				System.out.println(pn);
--- java/src/day22/practice/phone/vo/PhoneNumber.java
+++ java/src/day22/practice/phone/vo/PhoneNumber.java
@@ -5,36 +5,39 @@ import java.util.regex.Pattern;
 
 import lombok.Data;
 
-//한 사람의 전화번호
+//한사람의 전화번호 
 @Data
 public class PhoneNumber {
 	private String name;
-	private String number; // 010-1234-5678 // '-'가 들어가기때문에 String(문자열)로 정의
-
-	//생성자 -> 이 코드에서는 아래의 메서드 먼저 만든 후 생성자 만들기(checkNumber 때문에)
+	private String number; //010-1234-5678, 123456
+	
 	public PhoneNumber(String name, String number) {
 		this.name = name;
+		/*
 		this.number = "";
 		if(checkNumber(number)) {
 			this.number = number;
-		}
-		this.number = checkNumber(number)?number:""; //위의 3줄을 1줄로 줄이면 이렇게 씀
+		}*/
+		
+		this.number = checkNumber(number)?number:"";
+		//위 코드는 아래 주석으로 풀어 쓸수 있다.
+		/*if(checkNumber(number)) {
+			this.number = number;
+		}else {
+			this.number = "";
+		}*/
 	}
-	
 	
 	//전화번호가 010-xxxx-xxxx 패턴이 맞는지 확인해주는 메서드
-	//외부에서도 객체생성없이 쓸 수 있게 static과 public을 붙여줌
-	public static boolean checkNumber(String num){
-		String regex ="^010(-\\d{4}){2}$"; //https://regex101.com/ 에서 정규표현식 확인 후 붙여넣기
-		return Pattern.matches(regex, num); // 문자열이 정규표현식에 맞는지 아닌지 확인
+	//외부에서도 객체생성없이 쓸수 있게 static과 public을 붙여줌
+	public static boolean checkNumber(String num) {
+		String regex = "^010(-\\d{4}){2}$";
+		return Pattern.matches(regex, num);
 	}
 	
-	//전화번호를 새로 추가할 때 패턴에 맞는지 확인하기 위해 setter을 따로 만들어줌
-	//원래는 이 코드가 @Data에 자동으로 생성되지만 저장하기 전에 체크 넘버를 해줘야함
 	public void setNumber(String number) {
-		this.number = checkNumber(number)?number:this.number; // 위에서 쓴 빈문자열"" 대신 this.number를 씀
+		this.number = checkNumber(number)?number:this.number;
 	}
-
 
 	@Override
 	public boolean equals(Object obj) {
@@ -48,10 +51,9 @@ public class PhoneNumber {
 		return Objects.equals(name, other.name);
 	}
 
-
 	@Override
 	public int hashCode() {
 		return Objects.hash(name);
 	}
-
+		
 }
