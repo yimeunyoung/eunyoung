6b1580006bb7e228cf67a5edf06c89d2f9d4af3c
yimeunyoung <kil_yea@hanmail.net>
2023-08-16 17:41:19 +0900

    DB : 도서 대출 사이트 - 요구사항 및 DDL

--- db/31.도서 대출 사이트_요구사항 및 DDL.sql
+++ db/31.도서 대출 사이트_요구사항 및 DDL.sql
@@ -33,6 +33,19 @@ CREATE DATABASE BOOK;
 
 USE BOOK;
 
+DROP TABLE IF EXISTS `member`;
+
+CREATE TABLE `member` (
+	`me_id`	varchar(15)	NOT NULL PRIMARY KEY,
+	`me_pw`	varchar(255)	NOT NULL,
+	`me_phone`	char(13)	NOT NULL,
+	`me_birthday`	date	NOT NULL,
+	`me_authority`	varchar(5)	NOT NULL DEFAULT 'USER',
+	`me_book_count`	int	NOT NULL DEFAULT 0,
+	`me_total_overdue`	int	NOT NULL DEFAULT 0,
+	`me_apply_overdue`	date	NULL
+);
+
 DROP TABLE IF EXISTS `book`;
 
 CREATE TABLE `book` (
@@ -45,17 +58,15 @@ CREATE TABLE `book` (
 	`bo_possible_reservation`	int	NOT NULL
 );
 
-DROP TABLE IF EXISTS `member`;
+DROP TABLE IF EXISTS `loan`;
 
-CREATE TABLE `member` (
-	`me_id`	varchar(15)	NOT NULL PRIMARY KEY,
-	`me_pw`	varchar(255)	NOT NULL,
-	`me_phone`	char(13)	NOT NULL,
-	`me_birthday`	date	NOT NULL,
-	`me_authority`	varchar(5)	NOT NULL DEFAULT 'USER',
-	`me_book_count`	int	NOT NULL DEFAULT 0,
-	`me_total_overdue`	int	NOT NULL DEFAULT 0,
-	`me_apply_overdue`	date   NULL
+CREATE TABLE `loan` (
+	`lo_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
+	`lo_date`	datetime	NOT NULL,
+	`lo_me_id`	varchar(15)	NOT NULL,
+	`lo_bo_num`	varchar(20)	NOT NULL,
+	`lo_expected_date`	date	NOT NULL,
+	`lo_ return_date`	date	NULL
 );
 
 DROP TABLE IF EXISTS `reservation`;
@@ -67,24 +78,12 @@ CREATE TABLE `reservation` (
 	`re_bo_num`	varchar(20)	NOT NULL
 );
 
-DROP TABLE IF EXISTS `loan`;
-
-CREATE TABLE `loan` (
-	`lo_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
-	`lo_date`	datetime	NOT NULL,
-	`lo_me_id`	varchar(15)	NOT NULL,
-	`lo_bo_num`	varchar(20)	NOT NULL,
-	`lo_expected_date`	date	NOT NULL,
-	`lo_return_date`	date	NULL
-);
-
 DROP TABLE IF EXISTS `book_state`;
 
 CREATE TABLE `book_state` (
 	`bs_num`	int	NOT NULL PRIMARY KEY AUTO_INCREMENT,
 	`bs_desc`	varchar(20)	NOT NULL
 );
-
 
 ALTER TABLE `book` ADD CONSTRAINT `FK_book_state_TO_book_1` FOREIGN KEY (
 	`bo_possible_loan`
@@ -93,25 +92,11 @@ REFERENCES `book_state` (
 	`bs_num`
 );
 
-ALTER TABLE `book` ADD CONSTRAINT `FK_reservation_TO_book_1` FOREIGN KEY (
+ALTER TABLE `book` ADD CONSTRAINT `FK_book_state_TO_book_2` FOREIGN KEY (
 	`bo_possible_reservation`
 )
-REFERENCES `reservation` (
-	`re_num`
-);
-
-ALTER TABLE `reservation` ADD CONSTRAINT `FK_member_TO_reservation_1` FOREIGN KEY (
-	`re_me_id`
-)
-REFERENCES `member` (
-	`me_id`
-);
-
-ALTER TABLE `reservation` ADD CONSTRAINT `FK_book_TO_reservation_1` FOREIGN KEY (
-	`re_bo_num`
-)
-REFERENCES `book` (
-	`bo_num`
+REFERENCES `book_state` (
+	`bs_num`
 );
 
 ALTER TABLE `loan` ADD CONSTRAINT `FK_member_TO_loan_1` FOREIGN KEY (
@@ -128,3 +113,16 @@ REFERENCES `book` (
 	`bo_num`
 );
 
+ALTER TABLE `reservation` ADD CONSTRAINT `FK_member_TO_reservation_1` FOREIGN KEY (
+	`re_me_id`
+)
+REFERENCES `member` (
+	`me_id`
+);
+
+ALTER TABLE `reservation` ADD CONSTRAINT `FK_book_TO_reservation_1` FOREIGN KEY (
+	`re_bo_num`
+)
+REFERENCES `book` (
+	`bo_num`
+);
