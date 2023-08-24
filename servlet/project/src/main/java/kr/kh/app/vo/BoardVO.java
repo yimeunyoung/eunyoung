16b0cc14bee28ebc1495fd6ffc3fa3633c67ea24
yimeunyoung <kil_yea@hanmail.net>
2023-08-24 17:04:14 +0900

    Servlet : 게시글 수정 기능 구현 완료

--- servlet/project/src/main/java/kr/kh/app/vo/BoardVO.java
+++ servlet/project/src/main/java/kr/kh/app/vo/BoardVO.java
@@ -8,4 +8,15 @@ public class BoardVO {
 	private int bo_num;
 	private String bo_title;
 	private String bo_me_id;
+
+	public BoardVO(String title, String id) {
+		this.bo_title = title;
+		this.bo_me_id = id;
+	}
+
+	public BoardVO(int bo_num, String title, String id) {
+		this.bo_num = bo_num;
+		this.bo_title = title;
+		this.bo_me_id = id;
+	}
 }
