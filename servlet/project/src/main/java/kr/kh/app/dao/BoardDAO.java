d46d2ba9ac9433d7d96b9b294422fb9fb0a9fd15
yimeunyoung <kil_yea@hanmail.net>
2023-08-24 17:03:32 +0900

    Servlet : 게시글 수정 기능 구현 완료

--- servlet/project/src/main/java/kr/kh/app/dao/BoardDAO.java
+++ servlet/project/src/main/java/kr/kh/app/dao/BoardDAO.java
@@ -1,5 +1,21 @@
 package kr.kh.app.dao;
 
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+import kr.kh.app.vo.BoardVO;
+
 public interface BoardDAO {
+
+	void insertBoard(@Param("board")BoardVO board);
+
+	ArrayList<BoardVO> selectBoardList();
+
+	BoardVO selectBoard(@Param("bo_num")int bo_num);
+
+	int deleteBoard(@Param("bo_num")int bo_num);
+
+	int updateBoard(@Param("board")BoardVO board);
 
 }
