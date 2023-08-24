f2875ac1763f819c02830cf3e00c619ee093f7da
yimeunyoung <kil_yea@hanmail.net>
2023-08-24 17:03:01 +0900

    Servlet : 게시글 조회 기능 구현 완료

---
+++ servlet/project/src/main/java/kr/kh/app/controller/List.java
@@ -0,0 +1,33 @@
+package kr.kh.app.controller;
+
+import java.io.IOException;
+import java.util.ArrayList;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import kr.kh.app.service.BoardService;
+import kr.kh.app.service.BoardServiceImp;
+import kr.kh.app.vo.BoardVO;
+
+public class List extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private BoardService boardService = new BoardServiceImp();
+       
+    public List() {
+        super();
+    }
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<BoardVO> list = boardService.getBoardList();
+		request.setAttribute("list", list);
+		request.getRequestDispatcher("/WEB-INF/views/list.jsp").forward(request, response);
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		doGet(request, response);
+	}
+
+}
