9a4ee3ef860408c55ffc7cae83a2cce3e9dd2bee
yimeunyoung <kil_yea@hanmail.net>
2023-08-29 15:35:09 +0900

    SPRING : 메시지 클래스 추가 및 적용

--- spring projects/spring/src/main/java/kr/kh/spring/controller/MemberController.java
+++ spring projects/spring/src/main/java/kr/kh/spring/controller/MemberController.java
@@ -1,12 +1,18 @@
 package kr.kh.spring.controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import kr.kh.spring.service.MemberService;
+import kr.kh.spring.util.Message;
 import kr.kh.spring.vo.MemberVO;
 
 @Controller
@@ -15,23 +21,48 @@ public class MemberController {
 	@Autowired
 	private MemberService memberService;
 	
-	@RequestMapping(value="/signup", method=RequestMethod.GET)
+	@RequestMapping(value="/member/signup", method=RequestMethod.GET)
 	public String signup() {
 	
 		return "member/signup";
 	}
 	
-	@RequestMapping(value="/signup", method=RequestMethod.POST)
+	@RequestMapping(value="/member/signup", method=RequestMethod.POST)
 	public String signupPost(MemberVO member, Model model) {
-		String msg = "회원가입에 실패했습니다.";
-		String url = "/signup";
+		Message msg = new Message("/member/signup", "회원가입에 실패했습니다.");
+	
 		if(memberService.signup(member)) {
-			msg = "회원가입에 성공했습니다.";
-			url = "/";
-		}
-		
+			msg = new Message("/", "회원가입에 성공했습니다.");
+		}	
 		model.addAttribute("msg", msg);
-		model.addAttribute("url", url);
+		return "message";
+	}
+	@GetMapping(value="/member/login")
+	public String memberLogin() {
+		return "member/login";
+	}
+	@PostMapping(value="/member/login")
+	public String memberLoginPost(MemberVO member, Model model) {
+		Message msg = new Message("/member/login", "로그인에 실패했습니다.");
+	
+		MemberVO user = memberService.login(member);
+		if(user != null) {
+			msg = new Message("/", "로그인에 성공했습니다.");
+		}
+			model.addAttribute("user", user);
+			model.addAttribute("msg", msg);
+			return "message";
+	}
+	@GetMapping("/member/logout")
+	public String memberLogout(HttpServletRequest request, Model model) {
+		HttpSession session = request.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user");
+		Message msg = new Message("/", null);
+		if(user != null) {
+			session.removeAttribute("user");
+			msg.setMsg("로그아웃에 성공했습니다.");
+		}
+		model.addAttribute("msg", msg);
 		return "message";
 	}
 }
