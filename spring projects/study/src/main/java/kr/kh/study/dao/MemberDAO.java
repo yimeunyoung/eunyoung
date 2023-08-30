72564ee469bdcc59a2a5739426fcb940f5e273be
yimeunyoung <kil_yea@hanmail.net>
2023-08-30 12:39:52 +0900

    SPRING 복습 : 회원가입 기능 구현과 비밀번호 암호화 적용

--- spring projects/study/src/main/java/kr/kh/study/dao/MemberDAO.java
+++ spring projects/study/src/main/java/kr/kh/study/dao/MemberDAO.java
@@ -1,7 +1,15 @@
 package kr.kh.study.dao;
 
+import org.apache.ibatis.annotations.Param;
+
+import kr.kh.study.vo.MemberVO;
+
 public interface MemberDAO {
 
-	int selectCountMember();
+	MemberVO selectMember(@Param("me_id")String me_id);
+
+	boolean insertMember(@Param("m")MemberVO member); //member를 줄여서 m으로 씀
+
+
 
 }
