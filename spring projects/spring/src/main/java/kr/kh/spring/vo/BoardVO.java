9a1dd35c8b8e2206dd6a8cee246101b86fb1785e
yimeunyoung <kil_yea@hanmail.net>
2023-08-31 16:36:59 +0900

    SPRING : 게시글 상세 구현 완료 - 조회수 적용

--- spring projects/spring/src/main/java/kr/kh/spring/vo/BoardVO.java
+++ spring projects/spring/src/main/java/kr/kh/spring/vo/BoardVO.java
@@ -1,5 +1,6 @@
 package kr.kh.spring.vo;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 import lombok.Data;
@@ -9,4 +10,19 @@ public class BoardVO {
 	private int bo_num, bo_ori_num, bo_views, bo_up, bo_down, bo_bt_num, bo_comment;
 	private String bo_title, bo_me_id, bo_contents;
 	private Date bo_reg_date, bo_up_date;
+
+	public String getBo_up_date_str() {
+		if(bo_up_date == null) {
+			return "";
+		}
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		return format.format(bo_up_date);
+	}
+	public String getBo_reg_date_str() {
+		if(bo_reg_date == null) {
+			return "";
+		}
+		SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd hh:mm:ss");
+		return format.format(bo_reg_date);
+	}
 }
