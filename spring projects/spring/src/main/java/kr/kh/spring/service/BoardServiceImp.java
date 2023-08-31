b0dc3dcbb85b847929051159a9fc6e89f6f3e56f
yimeunyoung <kil_yea@hanmail.net>
2023-08-31 16:36:42 +0900

    SPRING : 게시글 상세 구현 완료 - 조회수 적용

--- spring projects/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
+++ spring projects/spring/src/main/java/kr/kh/spring/service/BoardServiceImp.java
@@ -1,9 +1,12 @@
 package kr.kh.spring.service;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import kr.kh.spring.dao.BoardDAO;
+import kr.kh.spring.pagination.Criteria;
 import kr.kh.spring.vo.BoardVO;
 import kr.kh.spring.vo.MemberVO;
 
@@ -23,5 +26,37 @@ public class BoardServiceImp implements BoardService{
 		}
 		board.setBo_me_id(user.getMe_id());
 		return boardDao.insertBoard(board);
+	}
+
+	@Override
+	public List<BoardVO> getBoardList(Criteria cri) {
+		if(cri == null) {
+			cri = new Criteria();
+		}
+		return boardDao.selectBoardList(cri);
+	}
+
+	@Override
+	public int getTotalCount(Criteria cri) {
+		if(cri == null) {
+			cri = new Criteria();
+		}
+		return boardDao.selectBoardCount(cri);
+	}
+
+	@Override
+	public BoardVO getBoard(Integer bo_num) {
+		if(bo_num == null) {
+		return null;
+	}
+	return boardDao.selectBoard(bo_num);
+	}
+
+	@Override
+	public void updateViews(Integer bo_num) {
+		if(bo_num == null) {
+			return;
+		}
+		boardDao.updateBoardViews(bo_num);
 	}
 }
