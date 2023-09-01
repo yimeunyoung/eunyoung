64f10c12a05045bfa6c26b5125e1b976aba74808
yimeunyoung <kil_yea@hanmail.net>
2023-09-01 14:13:46 +0900

    SPRING : 게시글 등록 이슈 해결

--- spring projects/spring/src/main/java/kr/kh/spring/vo/BoardVO.java
+++ spring projects/spring/src/main/java/kr/kh/spring/vo/BoardVO.java
@@ -11,9 +11,9 @@ public class BoardVO {
 	private int bo_num, bo_ori_num, bo_views, bo_up, bo_down, bo_bt_num, bo_comment;
 	private String bo_title, bo_me_id, bo_contents;
 	private Date bo_reg_date, bo_up_date;
-
-	private List<FileVO> files;
 	
+	private List<FileVO> fileVoList;
+
 	public String getBo_up_date_str() {
 		if(bo_up_date == null) {
 			return "";
