e194807a67cf77dd4b5dd0b7496c219c6f918052
yimeunyoung <kil_yea@hanmail.net>
2023-09-05 12:30:09 +0900

    SPRING 복습 : 게시글 등록, 조회, 삭제 - 첨부파일 적용

--- spring projects/study/src/main/java/kr/kh/study/service/BoardServiceImp.java
+++ spring projects/study/src/main/java/kr/kh/study/service/BoardServiceImp.java
@@ -1,19 +1,25 @@
 package kr.kh.study.service;
 
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
-import kr.kh.study.vo.BoardVO;
-import kr.kh.study.vo.MemberVO;
 import kr.kh.study.dao.BoardDAO;
+import kr.kh.study.util.UploadFileUtils;
+import kr.kh.study.vo.BoardVO;
+import kr.kh.study.vo.FileVO;
+import kr.kh.study.vo.MemberVO;
 
 @Service
 public class BoardServiceImp implements BoardService {
 
 	@Autowired
 	BoardDAO boardDao;
+	
+	String uploadPath = "D:\\uploadfiles";
 	
 	@Override
 	public List<BoardVO> getBoardList() {
@@ -32,6 +38,7 @@ public class BoardServiceImp implements BoardService {
 		}
 		//다오에게 게시글 번호를 주면서 게시글을 가져오라고 시킴
 		BoardVO board = boardDao.selectBoard(bo_num);
+		//가져오면 반환
 		return board;
 	}
 
@@ -46,16 +53,43 @@ public class BoardServiceImp implements BoardService {
 	}
 
 	@Override
-	public boolean insertBoard(BoardVO board, MemberVO user) {
+	public boolean insertBoard(BoardVO board, MemberVO user, MultipartFile[] files) {
 		if(user == null || user.getMe_id() == null) {
 			return false;
 		}
-		if(board == null || board.getBo_title()==null || board.getBo_title().length() == 0) {
+		if(board == null || board.getBo_title() == null || board.getBo_title().length() == 0) {
 			return false;
 		}
 		board.setBo_me_id(user.getMe_id());
 		boolean res = boardDao.insertBoard(board);
-			return res;
+		if(!res) {
+			return false;
+		}
+		//첨부파일 추가 
+		uploadFiles(files, board.getBo_num());
+		return true;
+	}
+
+	private void uploadFiles(MultipartFile[] files, int bo_num) {
+		if(files == null || files.length == 0) {
+			return;
+		}
+		if(bo_num <= 0) {
+			return;
+		}
+		for(MultipartFile file : files) {
+			if(file == null || file.getOriginalFilename().length() == 0) {
+				continue;
+			}
+			try {
+				String fi_ori_name = file.getOriginalFilename();
+				String fi_name = UploadFileUtils.uploadFile(uploadPath, fi_ori_name, file.getBytes());
+				FileVO fileVo = new FileVO(fi_name, fi_ori_name, bo_num);
+				boardDao.insertFile(fileVo);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 	}
 
 	@Override
@@ -63,7 +97,7 @@ public class BoardServiceImp implements BoardService {
 		if(user == null || user.getMe_id() == null) {
 			return false;
 		}
-		if(board == null || board.getBo_title()==null || board.getBo_title().length() == 0) {
+		if(board == null || board.getBo_title() == null || board.getBo_title().length() == 0) {
 			return false;
 		}
 		BoardVO dbBoard = boardDao.selectBoard(board.getBo_num());
@@ -73,7 +107,7 @@ public class BoardServiceImp implements BoardService {
 		boolean res = boardDao.updateBoard(board);
 		return res;
 	}
-
+	
 	@Override
 	public boolean deleteBoard(Integer bo_num, MemberVO user) {
 		if(user == null || user.getMe_id() == null) {
@@ -86,7 +120,37 @@ public class BoardServiceImp implements BoardService {
 		if(board == null || !board.getBo_me_id().equals(user.getMe_id())) {
 			return false;
 		}
+		//첨부파일을 삭제
+		//게시글의 모든 첨부파일들을 가져옴
+		List<FileVO> fileList = boardDao.selectFileList(bo_num);
+		//첨부파일 정보를 주면서 삭제하라고 요청
+		deleteFiles(fileList);
 		return boardDao.deleteBoard(bo_num);
+	}
+
+	private void deleteFiles(List<FileVO> fileList) {
+		//매개변수 예외처리
+		if(fileList == null || fileList.size() == 0) {
+			return;
+		}
+		//향상된 for문을 이용해서 하나씩 꺼냄
+		for(FileVO file :  fileList) {
+			// 혹시 모르니 예외처리도 진행
+			if(file == null) {
+				continue;
+			}
+			UploadFileUtils.deleteFile(uploadPath, file.getFi_name());
+			//삭제 후 db에서도 지우기
+			boardDao.deleteFile(file.getFi_num());
+		}
+	}
+
+	@Override
+	public List<FileVO> getFileList(Integer bo_num) {
+		if(bo_num == null) {
+			return null;
+		}
+		return boardDao.selectFileList(bo_num);
 	}
 	
 }
