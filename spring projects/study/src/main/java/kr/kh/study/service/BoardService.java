78ff7900f532932ec62e5165f663ee18cb334634
yimeunyoung <kil_yea@hanmail.net>
2023-09-05 17:28:51 +0900

    SPRING 복습 : 게시글 리스트 - 페이지네이션 적용

--- spring projects/study/src/main/java/kr/kh/study/service/BoardService.java
+++ spring projects/study/src/main/java/kr/kh/study/service/BoardService.java
@@ -4,13 +4,14 @@ import java.util.List;
 
 import org.springframework.web.multipart.MultipartFile;
 
+import kr.kh.study.pagination.Criteria;
 import kr.kh.study.vo.BoardVO;
 import kr.kh.study.vo.FileVO;
 import kr.kh.study.vo.MemberVO;
 
 public interface BoardService {
 
-	List<BoardVO> getBoardList();
+	List<BoardVO> getBoardList(Criteria cri);
 
 	BoardVO getBoard(Integer bo_num);
 
@@ -18,10 +19,12 @@ public interface BoardService {
 
 	boolean insertBoard(BoardVO board, MemberVO user, MultipartFile[] files);
 
-	boolean update(BoardVO board, MemberVO user);
+	boolean update(BoardVO board, MemberVO user, MultipartFile[] files, int[] delNums);
 
 	boolean deleteBoard(Integer bo_num, MemberVO user);
 
 	List<FileVO> getFileList(Integer bo_num);
+
+	int getBoardTotalCount();
 
 }
