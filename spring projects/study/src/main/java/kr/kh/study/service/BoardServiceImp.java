439b836496ef4bcc69a7ef3a4e07debbca24900a
yimeunyoung <kil_yea@hanmail.net>
2023-09-05 17:29:19 +0900

    SPRING 복습 : 게시글 리스트 - 페이지네이션 적용

--- spring projects/study/src/main/java/kr/kh/study/service/BoardServiceImp.java
+++ spring projects/study/src/main/java/kr/kh/study/service/BoardServiceImp.java
@@ -1,6 +1,8 @@
 package kr.kh.study.service;
 
+import java.io.File;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,31 +10,35 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import kr.kh.study.dao.BoardDAO;
+import kr.kh.study.pagination.Criteria;
 import kr.kh.study.util.UploadFileUtils;
 import kr.kh.study.vo.BoardVO;
 import kr.kh.study.vo.FileVO;
 import kr.kh.study.vo.MemberVO;
 
 @Service
-public class BoardServiceImp implements BoardService {
+public class BoardServiceImp implements BoardService{
 
 	@Autowired
 	BoardDAO boardDao;
 	
 	String uploadPath = "D:\\uploadfiles";
-	
+
 	@Override
-	public List<BoardVO> getBoardList() {
-		//매개변수 체크(없기 때문에 생략)
+	public List<BoardVO> getBoardList(Criteria cri) {
+		//매개변수체크
+		if(cri == null) {
+			cri = new Criteria();
+		}
 		//다오에게 게시글 리스트를 가져오라고 시키고
-		List<BoardVO> list = boardDao.selectBoardList();
+		List<BoardVO> list = boardDao.selectBoardList(cri);
 		//가져오면 반환
 		return list;
 	}
 
 	@Override
 	public BoardVO getBoard(Integer bo_num) {
-		//매개변수 체크
+		//매개변수체크
 		if(bo_num == null) {
 			return null;
 		}
@@ -46,9 +52,9 @@ public class BoardServiceImp implements BoardService {
 	public void updateViews(Integer bo_num) {
 		//매개변수 체크
 		if(bo_num == null) {
-			return; //void이기때문에 return null이 아닌 그냥 return만 쓰기
+			return;
 		}
-		//다오에게 게시글 번호를 주면서 조회수를 1증가 시키라고 요청
+		//다오에게 게시글 번호를 주면서 조회수를 1증가시키리고 요청
 		boardDao.updateBoardViews(bo_num);
 	}
 
@@ -93,7 +99,7 @@ public class BoardServiceImp implements BoardService {
 	}
 
 	@Override
-	public boolean update(BoardVO board, MemberVO user) {
+	public boolean update(BoardVO board, MemberVO user, MultipartFile[] files, int[] delNums) {
 		if(user == null || user.getMe_id() == null) {
 			return false;
 		}
@@ -104,10 +110,26 @@ public class BoardServiceImp implements BoardService {
 		if(dbBoard == null || !dbBoard.getBo_me_id().equals(user.getMe_id())) {
 			return false;
 		}
+		//추가된 첨부파일 업로드 및 DB 추가
+		uploadFiles(files, board.getBo_num());
+		//삭제된 첨부파일 삭제 및 DB 제거
+		deleteFiles(delNums);
 		boolean res = boardDao.updateBoard(board);
 		return res;
 	}
-	
+
+	private void deleteFiles(int[] delNums) {
+		if(delNums == null || delNums.length == 0) {
+			return ;
+		}
+		List<FileVO> fileList = new ArrayList<FileVO>();
+		for(int fi_num : delNums) {
+			FileVO fileVo = boardDao.selectFile(fi_num);
+			fileList.add(fileVo);
+		}
+		deleteFiles(fileList);
+	}
+
 	@Override
 	public boolean deleteBoard(Integer bo_num, MemberVO user) {
 		if(user == null || user.getMe_id() == null) {
@@ -120,29 +142,26 @@ public class BoardServiceImp implements BoardService {
 		if(board == null || !board.getBo_me_id().equals(user.getMe_id())) {
 			return false;
 		}
-		//첨부파일을 삭제
-		//게시글의 모든 첨부파일들을 가져옴
+		//첨부파일을 삭제 
+		//게시글의 모든 첨부파일들을 가져옴 
 		List<FileVO> fileList = boardDao.selectFileList(bo_num);
-		//첨부파일 정보를 주면서 삭제하라고 요청
+		//첨부파일 정보를 주면서 삭제하라고 요청 
 		deleteFiles(fileList);
 		return boardDao.deleteBoard(bo_num);
 	}
 
 	private void deleteFiles(List<FileVO> fileList) {
-		//매개변수 예외처리
 		if(fileList == null || fileList.size() == 0) {
 			return;
 		}
-		//향상된 for문을 이용해서 하나씩 꺼냄
-		for(FileVO file :  fileList) {
-			// 혹시 모르니 예외처리도 진행
+		for(FileVO file : fileList) {
 			if(file == null) {
 				continue;
 			}
 			UploadFileUtils.deleteFile(uploadPath, file.getFi_name());
-			//삭제 후 db에서도 지우기
 			boardDao.deleteFile(file.getFi_num());
 		}
+		
 	}
 
 	@Override
@@ -152,5 +171,11 @@ public class BoardServiceImp implements BoardService {
 		}
 		return boardDao.selectFileList(bo_num);
 	}
+
+	@Override
+	public int getBoardTotalCount() {
+		return boardDao.selectBoardTotalCount();
+	}
+	
 	
 }
