ebdc8abd2ce2d5fd3e6fec89088b309a3b7fc7d9
yimeunyoung <kil_yea@hanmail.net>
2023-09-07 10:27:29 +0900

    SPRING : 댓글 조회 이슈 해결

--- spring projects/spring/src/main/java/kr/kh/spring/controller/CommentController.java
+++ spring projects/spring/src/main/java/kr/kh/spring/controller/CommentController.java
@@ -1,13 +1,16 @@
 package kr.kh.spring.controller;
 
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import kr.kh.spring.pagination.Criteria;
 import kr.kh.spring.service.CommentService;
 import kr.kh.spring.vo.CommentVO;
 
@@ -25,5 +28,12 @@ public class CommentController {
 		return map;
 	}
 	
+	@PostMapping("/comment/list/{bo_num}")
+	public Map<String, Object> list(@RequestBody Criteria cri, @PathVariable("bo_num")int bo_num){
+		Map<String, Object> map = new HashMap<String, Object>();
+		List<CommentVO> list = commentService.getCommentList(cri, bo_num);
+		map.put("list", list);
+		return map;
+	}
 	
 }
