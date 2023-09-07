f7c18c7b7631edf8f46f3c44766d2f23abe32087
yimeunyoung <kil_yea@hanmail.net>
2023-09-07 12:30:04 +0900

    SPRING : 댓글 등록/삭제 시 게시글에 댓글 수 변경 적용

--- spring projects/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
+++ spring projects/spring/src/main/java/kr/kh/spring/service/CommentServiceImp.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import kr.kh.spring.dao.BoardDAO;
 import kr.kh.spring.dao.CommentDAO;
 import kr.kh.spring.pagination.Criteria;
 import kr.kh.spring.vo.CommentVO;
@@ -14,14 +15,20 @@ public class CommentServiceImp implements CommentService {
 
 	@Autowired
 	CommentDAO commentDao;
+	@Autowired
+	BoardDAO boardDao;
 	
 	@Override
 	public boolean insertComment(CommentVO comment) {
 		if(comment == null || comment.getCo_contents() == null || comment.getCo_me_id() == null) {
 			return false;
 		}
-		
-		return commentDao.insertComment(comment);
+		boolean res = commentDao.insertComment(comment);
+		if(!res) {
+			return false;
+		}
+		boardDao.updateBoardComment(comment.getCo_bo_num());
+		return true; 
 	}
 
 	@Override
@@ -35,6 +42,19 @@ public class CommentServiceImp implements CommentService {
 	@Override
 	public int getTotalCount(int bo_num) {
 		return commentDao.selectCommentCount(bo_num);
+	}
+
+	@Override
+	public boolean deleteComment(CommentVO comment) {
+		if(comment == null || comment.getCo_me_id() == null) {
+			return false;
+		}
+		boolean res = commentDao.deleteComment(comment);
+		if(!res) {
+			return false;
+		}
+		boardDao.updateBoardComment(comment.getCo_bo_num());
+		return true;
 	}
 
 }
