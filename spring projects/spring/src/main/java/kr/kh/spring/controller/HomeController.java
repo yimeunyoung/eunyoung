3204ff29cbb0566871c5fb3a7e689496085e9cd2
yimeunyoung <kil_yea@hanmail.net>
2023-09-11 12:43:53 +0900

    SPRING : ajax 샘플 코드

--- spring projects/spring/src/main/java/kr/kh/spring/controller/HomeController.java
+++ spring projects/spring/src/main/java/kr/kh/spring/controller/HomeController.java
@@ -17,15 +17,13 @@ public class HomeController {
 	
 	@RequestMapping(value="/")
 	public String home(Model model) {
-		//화면에 데이터를 전송하는 방법 model에 담아서 전송
-		/*model.addAttribute("화면에서 사용할 이름", 전송할값);*/
 		model.addAttribute("name", "abc");
 		return "/main/home";
 	}
 	
 	@RequestMapping(value="/test1")
 	public String test1(Model model, String id, Integer age) {
-		System.out.println("아이디: " +id);
+		System.out.println("아이디 : " +id);
 		System.out.println("나이 : " + age);
 		return "test1";
 	}
